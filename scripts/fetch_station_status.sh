b731af15d286c63a566e5b286bf99bb2a6092e50
Michael Dür <62062370+michael-duer@users.noreply.github.com>
2026-05-20 21:18:31 +0200

    Update scripts for new project structure
    - Adjust collection and processing scripts to use updated data and log paths
    - Improve API request robustness with increased curl retries and timeouts
    - Add example raw NDJSON file and processed bike availability dataset
    - Update README with data availability section
    - Update gitignore to ignore all generated intermediate files from prepare_bike_dataset.sh

--- scripts/fetch_station_status.sh
+++ scripts/fetch_station_status.sh
@@ -11,11 +11,14 @@ echo " Start data collection..."
 AUTH="?Authorization=${EMAIL_ADDRESS}"
 URL="https://sharedmobility.ch/v2/gbfs/velospot/station_status$AUTH"
 
-DATA_FILE="data/$(date +%F).ndjson" # Create file for each day (e.g. 2026-05-05.ndjson)
-ERROR_LOG="collector.log"
+DATA_FILE="data/raw/$(date +%F).ndjson" # Create file for each day (e.g. 2026-05-05.ndjson)
+LOG_FILE="logs/station_status_collection.log"
 
-MAX_RETRIES=3
-RETRY_DELAY=10
+# Create folders if they do not already exist
+mkdir -p data/raw logs
+
+MAX_RETRIES=5
+RETRY_DELAY=15
 
 response=$(
   curl \
@@ -26,25 +29,28 @@ response=$(
     --retry "$MAX_RETRIES" \
     --retry-delay "$RETRY_DELAY" \
     --retry-all-errors \
-    "$URL" 2>> "$ERROR_LOG"
+    --connect-timeout 20 \
+    --max-time 120 \
+    "$URL" 2>> "$LOG_FILE"
 )
 
 curl_exit=$? # Uses exit status of the last command that was executed
 
 # If http request fails, write error in log file
 if [ "$curl_exit" -ne 0 ]; then # -ne = not equal
-  echo "ERROR: curl failed with exit code $curl_exit" >> "$ERROR_LOG"
+  echo "$(date -Iseconds) ERROR: curl failed with exit code $curl_exit" >> "$LOG_FILE"
   exit 1
 fi
 
 # Write an error message in the log file if the request succeeds but the json parsing fails
-if ! echo "$response" | jq -e . > /dev/null 2>> "$ERROR_LOG"; then
-  echo "ERROR: invalid JSON response" >> "$ERROR_LOG"
-  echo "$response" >> "$ERROR_LOG"
+if ! echo "$response" | jq -e . > /dev/null 2>> "$LOG_FILE"; then
+  echo "$(date -Iseconds) ERROR: invalid JSON response" >> "$LOG_FILE"
+  echo "$response" >> "$LOG_FILE"
   exit 1
 fi
 
 # If the response is valid JSON, append it to the data file
 echo "$response" | jq -c "." >> "$DATA_FILE"
+echo "$(date -Iseconds) Ok: appended data to $DATA_FILE" >> "$LOG_FILE"
 
 echo "Script finished"
